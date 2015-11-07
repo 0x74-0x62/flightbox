c6713894a08e98e0959ccd6868ad2eb432e77df0
Thorsten Biermann <thorsten.biermann@gmail.com>
2015-11-07 17:10:34 +0100

    Makes GNSS input more robust (supports re-connecting device)

--- input/input_serial_gnss.py
+++ input/input_serial_gnss.py
@@ -1,5 +1,6 @@
 import logging
 import serial
+import time
 
 from data_hub.data_hub_item import DataHubItem
 from input.input_module import InputModule
@@ -25,23 +26,42 @@ class InputSerialGnss(InputModule):
     def run(self):
         self._logger.info('Running')
 
-        # create serial object
-        s = serial.Serial(self._port, self._baud_rate)
+        # initialize serial object
+        s = None
 
-        try:
-            while True:
-                # get line from serial device (blocking call)
-                line = s.readline().decode().strip()
+        while True:
+            try:
+                # wait before attaching to serial port
+                time.sleep(5)
 
-                self._logger.debug('Data received: {!r}'.format(line))
+                # create serial object
+                s = serial.Serial(self._port, self._baud_rate)
 
-                # generate new data hub item and hand over to data hub
-                data_hub_item = DataHubItem('nmea', line)
-                self._data_hub.put(data_hub_item)
-        except(KeyboardInterrupt, SystemExit):
-            pass
-        finally:
-            s.close()
+                # read loop
+                while True:
+                    try:
+                        # get line from serial device (blocking call)
+                        line = s.readline().decode().strip()
+                    except:
+                        # in case read was unsuccessful, exit read loop
+                        break
+
+                    self._logger.debug('Data received: {!r}'.format(line))
+
+                    # generate new data hub item and hand over to data hub
+                    data_hub_item = DataHubItem('nmea', line)
+                    self._data_hub.put(data_hub_item)
+            except(KeyboardInterrupt, SystemExit):
+                # exit re-connect loop in case of termination is requested
+                break
+            except:
+                self._logger.warning('Could not attach to serial port {} with baud rate {:d}'.format(self._port, self._baud_rate))
+
+                # continue in any other exception case
+                pass
+            finally:
+                if s:
+                    s.close()
 
         # close data input queue
         self._data_hub.close()
